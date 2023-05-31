1020cf9aef349a563cb89916a5f6dfee19bd76c4
hryhorii11 <124711455+hryhorii11@users.noreply.github.com>
2023-06-01 00:12:31 +0300

    Update script.js

--- Lab9/script.js
+++ Lab9/script.js
@@ -7,14 +7,14 @@ const route = (event) => {
 
 const routes = {
 
-    "/webclass/Lab9/": "/web/Lab9/index.html",
-    "/webclass/Lab9/catalog": "/web/Lab9/pages/catalog.html",
-    "/webclass/Lab9/catalog/1": "/web/Lab9/pages/catalog_data.html",
-    "/webclass/Lab9/catalog/2": "/web/Lab9/pages/catalog_data.html",
-    "/webclass/Lab9/catalog/3": "/web/Lab9/pages/catalog_data.html",
-    "/webclass/Lab9/catalog/4": "/web/Lab9/pages/catalog_data.html",
-    "/webclass/Lab9/catalog/5": "/web/Lab9/pages/catalog_data.html",
-    "/webclass/Lab9/catalog/6": "/web/Lab9/pages/catalog_data.html",
+    "/web/Lab9/": "/web/Lab9/index.html",
+    "/web/Lab9/catalog": "/web/Lab9/pages/catalog.html",
+    "/web/Lab9/catalog/1": "/web/Lab9/pages/catalog_data.html",
+    "/web/Lab9/catalog/2": "/web/Lab9/pages/catalog_data.html",
+    "/web/Lab9/catalog/3": "/web/Lab9/pages/catalog_data.html",
+    "/web/Lab9/catalog/4": "/web/Lab9/pages/catalog_data.html",
+    "/web/Lab9/catalog/5": "/web/Lab9/pages/catalog_data.html",
+    "/web/Lab9/catalog/6": "/web/Lab9/pages/catalog_data.html",
 };
 const endsWithNumber = (text) => {
     return /\d$/.test(text);
@@ -26,7 +26,7 @@ const handleLocation = async () => {
     const path = window.location.pathname;
     const route = routes[path];
     
-    if (path === "/webclass/Lab9/") {
+    if (path === "/web/Lab9/") {
         const html = await fetch('/web/Lab9/index.html').then((data) => data.text());
         document.getElementsByTagName("html")[0].innerHTML = html;
     }
@@ -82,7 +82,7 @@ const handleLocation = async () => {
 
             for (const item of data.data) {
                 dataText += `<div class="flex flex-col">
-                <a href="/webclass/Lab9/catalog/${item.id}" onclick="route()" class="relative inline-flex overflow-hidden p-4 w-full h-full m-auto mb-2">
+                <a href="/web/Lab9/catalog/${item.id}" onclick="route()" class="relative inline-flex overflow-hidden p-4 w-full h-full m-auto mb-2">
                   <div class="w-full h-full absolute left-0 top-0 z-10 border-white border-2"></div>
                   <img src="${item.image}" class="w-full h-full z-0 opacity-90 object-cover" alt="">
                   <div class="absolute left-0 bottom-0 z-30 p-7 h-48 overflow-hidden w-full">
@@ -90,10 +90,10 @@ const handleLocation = async () => {
                     <p class="text-sm">Notes: ${item.notes}</p>
                   </div>
                 </a>
-                <a href="/webclass/Lab9/catalog/${item.id}" class="inline-block px-6 py-3 font-semibold bg-gradient-to-r from-purple-500 to-indigo-500 rounded-full shadow-md hover:bg-gradient-to-r hover:from-indigo-500 hover:to-purple-500 hover:shadow-lg transition duration-300 ease-in-out" onclick="route()">Go to</a>
+                <a href="/web/Lab9/catalog/${item.id}" class="inline-block px-6 py-3 font-semibold bg-gradient-to-r from-purple-500 to-indigo-500 rounded-full shadow-md hover:bg-gradient-to-r hover:from-indigo-500 hover:to-purple-500 hover:shadow-lg transition duration-300 ease-in-out" onclick="route()">Go to</a>
               </div>`
             }
-            dataText += `<a class="inline-flex col-span-3 justify-center items-center p-2 w-full font-bold bg-red-500 rounded-lg shadow-md hover:bg-red-600 transition duration-300 ease-in-out" href="/webclass/Lab9/catalog/${getRandomInt(data.data.length)}" onclick="route()">Random Catalog</a>
+            dataText += `<a class="inline-flex col-span-3 justify-center items-center p-2 w-full font-bold bg-red-500 rounded-lg shadow-md hover:bg-red-600 transition duration-300 ease-in-out" href="/web/Lab9/catalog/${getRandomInt(data.data.length)}" onclick="route()">Random Catalog</a>
             `;
             document.getElementById("app").innerHTML = html;
             document.getElementById('catalog-section').innerHTML = dataText
